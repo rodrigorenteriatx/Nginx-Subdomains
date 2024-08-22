9f9f5e3efcb00073a31c3057363afdf0ea1d3b50
Rodrigo Renteria <98871709+rodrigorenteriatx@users.noreply.github.com>
2024-08-22 08:12:45 -0700

    Added inline expect script

--- acme-dns-build.sh
+++ acme-dns-build.sh
@@ -53,7 +53,35 @@ ssh $USER@$HOST "sudo acme-dns-client register -d rodrigonginx.com -s http://loc
 # We will need to read output from above command for the record and CNAME value.
 #Then we can continue with script
 
-./expect_certbot.sh
+#./expect_certbot.sh
+
+expect << "EOF"
+set timeout -1
+#set password "your-password"  # if SSH password is required, consider using SSH keys instead
+
+spawn ssh $USER@$HOST
+# expect "*assword:*"
+# send "$password\r"
+
+# expect "*\$*"
+# This line tells Expect to wait until it sees the shell prompt ($), which indicates that the SSH session has successfully logged in and the shell is ready to accept commands.
+
+expect "*\$*"
+send "sudo certbot register -m rodrigorenteriatx@gmail.com --agree-tos\r"
+
+expect "*Do you agree to the terms?*"
+send "n\r"
+
+expect eof
+EOF
+
+#     Ensuring Command Execution: Without the carriage return, the command might just be typed out without being executed. Adding \r ensures that the command is actually submitted to the system for execution.
+
+# Examples of Carriage Return in Action:
+
+#     Sending a Command via Expect:
+#         send "sudo apt update\r"
+#         In this line, "sudo apt update" is the command you want to run, and \r tells the terminal to execute the command as if you had typed it and pressed Enter.
 
 #MANUAL REGISTRATION
 #sudo certbot certonly --manual --preferred-challenges dns --manual-auth-hook 'acme-dns-client' -d *.rodrigonginx.com
